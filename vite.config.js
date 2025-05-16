52d320de8ff8d093355839a3cb0cac99df0a2636
luffrey1 <tu_email@ejemplo.com>
2025-05-16 19:44:53 +0200

    cambios para github

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,14 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
   },
   server: {
     open: true
